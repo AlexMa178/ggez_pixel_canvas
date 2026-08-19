eca7449bacd53c4adc3df8cf2228160478702e07
AlexMa178 <alexcma27@gmail.com>
2026-08-18 19:38:32 -0700

    use glamour and remove own Rectangle because glamour has one

--- src/builders.rs
+++ src/builders.rs
@@ -1,18 +1,16 @@
-use std::ops::Mul;
-
 use ggez::graphics::ZIndex;
-use ggez::mint::{ Point2, Vector2 };
 
-use num_traits::{ NumCast, ToPrimitive, Zero };
+use glamour::{ Point2, Rect, Unit };
 
 use generic_discrete_2d_rotations::Angle;
+use num_traits::{ AsPrimitive, ConstZero, NumCast };
 
-use crate::{ Rectangle, AtlasSection, PixelDrawParams };
+use crate::{ AtlasSection, PixelDrawParams };
 
-pub struct PDPBuilder<P> {
+pub struct PDPBuilder<P: Unit> {
     pdp: PixelDrawParams<P>
 }
-impl<P: Zero> PDPBuilder<P> {
+impl<P: Unit> PDPBuilder<P> {
 
     pub fn new() -> Self {
         Self { pdp: PixelDrawParams::default() }
@@ -30,31 +28,32 @@ impl<P: Zero> PDPBuilder<P> {
         self.pdp.z = z; self
     }
 
-    pub fn dest(mut self, dest: impl Into<Point2<P>>) -> Self {
-        self.pdp.dest = dest.into(); self
+    pub fn dest(mut self, dest: impl Into<[ P::Scalar; 2 ]>) -> Self {
+        self.pdp.dest = Point2::from_array(dest.into()); self
     }
 
-    pub fn atlas_rect(mut self, atlas_rect: impl Into<Rectangle<P>>) -> Self {
-        self.pdp.atlas_section = AtlasSection::Rect { rect: atlas_rect.into() }; self
+    pub fn atlas_rect(mut self, atlas_rect: impl Into<[ P::Scalar; 4 ]>) -> Self {
+        let [ x, y, w, h ] = atlas_rect.into();
+        self.pdp.atlas_section = AtlasSection::Rect { rect: Rect::from_origin_and_size([ x, y ], [ w, h ]) }; self
     }
 
-    pub fn anchor(mut self, anchor: impl Into<Point2<P>>) -> Self {
-        self.pdp.anchor = anchor.into(); self
+    pub fn anchor(mut self, anchor: impl Into<[ P::Scalar; 2 ]>) -> Self {
+        self.pdp.anchor = Point2::from_array(anchor.into()); self
     }
 
-    pub fn pivot(mut self, pivot: impl Into<Point2<P>>) -> Self {
-        self.pdp.pivot = pivot.into(); self
+    pub fn pivot(mut self, pivot: impl Into<[ P::Scalar; 2 ]>) -> Self {
+        self.pdp.pivot = Point2::from_array(pivot.into()); self
     }
 
 }
 
-pub struct PDPTileBuilder<P> {
+pub struct PDPTileBuilder<P: Unit> {
     builder: PDPBuilder<P>,
-    tile_size: u8,
+    tile_size: P::Scalar,
 }
-impl<P: Zero + NumCast + Mul<Output = P> + Copy> PDPTileBuilder<P> {
+impl<P: Unit> PDPTileBuilder<P> {
 
-    pub fn new(tile_size: u8) -> Self {
+    pub fn new(tile_size: P::Scalar) -> Self {
         Self { builder: PDPBuilder::new(), tile_size }
     }
 
@@ -70,58 +69,58 @@ impl<P: Zero + NumCast + Mul<Output = P> + Copy> PDPTileBuilder<P> {
         Self { builder: self.builder.z(z), ..self }
     }
 
-    pub fn pixel_dest(self, dest: impl Into<Point2<P>>) -> Self {
+    pub fn pixel_dest(self, dest: impl Into<[ P::Scalar; 2 ]>) -> Self {
         Self { builder: self.builder.dest(dest), ..self }
     }
 
-    pub fn pixel_atlas_rect(self, atlas_rect: impl Into<Rectangle<P>>) -> Self {
+    pub fn pixel_atlas_rect(self, atlas_rect: impl Into<[ P::Scalar; 4 ]>) -> Self {
         Self { builder: self.builder.atlas_rect(atlas_rect), ..self }
     }
 
-    pub fn pixel_anchor(self, anchor: impl Into<Point2<P>>) -> Self {
+    pub fn pixel_anchor(self, anchor: impl Into<[ P::Scalar; 2 ]>) -> Self {
         Self { builder: self.builder.anchor(anchor), ..self }
     }
 
-    pub fn pixel_pivot(self, pivot: impl Into<Point2<P>>) -> Self {
+    pub fn pixel_pivot(self, pivot: impl Into<[ P::Scalar; 2 ]>) -> Self {
         Self { builder: self.builder.pivot(pivot), ..self }
     }
 
-    pub fn tile_dest<T: ToPrimitive>(self, dest: impl Into<Point2<T>>) -> Self {
-        let ts = P::from(self.tile_size).unwrap();
-        let Point2 { x, y } = dest.into();
+    pub fn tile_dest<T: Unit<Scalar: AsPrimitive<P::Scalar>>>(self, dest: impl Into<[ T::Scalar; 2 ]>) -> Self {
+        let ts = self.tile_size;
+        let [ x, y ] = dest.into();
         self.pixel_dest([
-            P::from(x).unwrap() * ts,
-            P::from(y).unwrap() * ts,
+            x.as_() * ts,
+            y.as_() * ts,
         ])
     }
 
-    pub fn tile_atlas_rect<T: ToPrimitive>(self, atlas_rect: impl Into<Rectangle<T>>) -> Self {
-        let ts = P::from(self.tile_size).unwrap();
-        let Rectangle { pos: Point2 { x, y }, dim: Vector2 { x: w, y: h } } = atlas_rect.into();
+    pub fn tile_atlas_rect<T: Unit<Scalar: AsPrimitive<P::Scalar>>>(self, atlas_rect: impl Into<[ T::Scalar; 4 ]>) -> Self {
+        let ts = self.tile_size;
+        let [ x, y, w, h ] = atlas_rect.into();
         self.pixel_atlas_rect([
-            P::from(x).unwrap() * ts,
-            P::from(y).unwrap() * ts,
-            P::from(w).unwrap() * ts,
-            P::from(h).unwrap() * ts,
+            x.as_() * ts,
+            y.as_() * ts,
+            w.as_() * ts,
+            h.as_() * ts,
         ])
     }
 
-    pub fn tile_anchor<T: ToPrimitive>(self, anchor: impl Into<Point2<T>>) -> Self {
-        let ts = P::from(self.tile_size).unwrap();
-        let Point2 { x, y } = anchor.into();
+    pub fn tile_anchor<T: Unit<Scalar: AsPrimitive<P::Scalar>>>(self, anchor: impl Into<[ T::Scalar; 2 ]>) -> Self {
+        let ts = self.tile_size;
+        let [ x, y ] = anchor.into();
         self.pixel_anchor([
-            P::from(x).unwrap() * ts,
-            P::from(y).unwrap() * ts,
+            x.as_() * ts,
+            y.as_() * ts,
         ])
     }
 
-    pub fn tile_pivot<T: ToPrimitive>(self, pivot: impl Into<Point2<T>>, x_odd: bool, y_odd: bool) -> Self {
-        let ts = P::from(self.tile_size).unwrap();
-        let half = P::from(self.tile_size / 2).unwrap();
-        let Point2 { x, y } = pivot.into();
+    pub fn tile_pivot<T: Unit<Scalar: AsPrimitive<P::Scalar>>>(self, pivot: impl Into<[ T::Scalar; 2 ]>, x_odd: bool, y_odd: bool) -> Self {
+        let ts = self.tile_size;
+        let half = ts / NumCast::from(2).unwrap();
+        let [ x, y ] = pivot.into();
         self.pixel_pivot([
-            P::from(x).unwrap() * ts + if x_odd { half } else { P::zero() },
-            P::from(y).unwrap() * ts + if y_odd { half } else { P::zero() },
+            x.as_() * ts + if x_odd { half } else { ConstZero::ZERO },
+            y.as_() * ts + if y_odd { half } else { ConstZero::ZERO },
         ])
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,36 +2,24 @@ mod builders;
 pub use builders::*;
 
 use ggez::error::GameResult;
-use ggez::graphics::{ Canvas, Color, DrawParam, GraphicsContext, Image, Rect, Sampler, Transform, ZIndex };
+use ggez::graphics::{ Canvas, Color, DrawParam, GraphicsContext, Image, Rect as GraphicsRect, Sampler, ZIndex };
 use ggez::context::{ Has, HasMut };
 
-use ggez::mint::{ Point2, Vector2 };
+use glamour::{ Point2, Rect, Size2, Unit };
 
-use num_traits::{ NumCast, Zero };
+use num_traits::AsPrimitive;
 
 pub use generic_discrete_2d_rotations as rotation;
 use rotation::Angle;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Rectangle<T> {
-    pub pos: Point2<T>,
-    pub dim: Vector2<T>,
-}
-impl<T, V: Into<[ T; 4 ]>> From<V> for Rectangle<T> {
-    fn from(value: V) -> Self {
-        let [ x, y, w, h ] = value.into();
-        Rectangle { pos: Point2 { x, y }, dim: Vector2 { x: w, y: h } }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum AtlasSection<P> {
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum AtlasSection<P: Unit> {
     All,
-    Rect { rect: Rectangle<P> },
+    Rect { rect: Rect<P> },
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct PixelDrawParams<P> {
+#[derive(Debug, Clone, PartialEq)]
+pub struct PixelDrawParams<P: Unit> {
     pub dest: Point2<P>,
     pub atlas_section: AtlasSection<P>,
     pub angle: Angle<4>,
@@ -39,14 +27,14 @@ pub struct PixelDrawParams<P> {
     pub pivot: Point2<P>,
     pub z: ZIndex,
 }
-impl<P: Zero> Default for PixelDrawParams<P> {
+impl<P: Unit> Default for PixelDrawParams<P> {
     fn default() -> Self {
         Self {
             atlas_section: AtlasSection::All,
-            dest: Point2 { x: P::zero(), y: P::zero() },
+            dest: Point2::ZERO,
             angle: Angle::A4_0,
-            anchor: Point2 { x: P::zero(), y: P::zero() },
-            pivot: Point2 { x: P::zero(), y: P::zero() },
+            anchor: Point2::ZERO,
+            pivot: Point2::ZERO,
             z: 0,
         }
     }
@@ -68,54 +56,44 @@ impl PixelCanvas {
         self.canvas.finish(gfx)
     }
 
-    pub fn draw<P: NumCast>(&mut self, image: &Image, params: PixelDrawParams<P>) {
+    pub fn draw<P: Unit<Scalar: AsPrimitive<f32>>>(&mut self, image: &Image, params: PixelDrawParams<P>) {
 
-        let f = |p: P| p.to_f32().unwrap();
-
-        let image_dim = Vector2 {
-            x: image.width()  as f32,
-            y: image.height() as f32,
-        };
+        let image_size_f = Size2::<u32> {
+            width: image.width(),
+            height: image.height(),
+        }.as_::<f32>();
 
         let ps = self.pixel_size as f32;
 
         let PixelDrawParams { dest, atlas_section, angle, anchor, pivot, z } = params;
 
-        let atlas_rect = match atlas_section {
-            AtlasSection::All => Rectangle { pos: Point2 { x: 0., y: 0. }, dim: image_dim },
-            AtlasSection::Rect { rect } => Rectangle {
-                pos: Point2  { x: f(rect.pos.x), y: f(rect.pos.y) },
-                dim: Vector2 { x: f(rect.dim.x), y: f(rect.dim.y) },
-            },
+        let dest_f = dest.as_::<f32>();
+        let pivot_f = pivot.as_::<f32>();
+        let anchor_f = anchor.as_::<f32>();
+
+        let atlas_rect_f = match atlas_section {
+            AtlasSection::All => Rect { origin: Point2::ZERO, size: image_size_f },
+            AtlasSection::Rect { rect } => rect.as_::<f32>(),
         };
 
-        let pivot = Point2 {
-            x: f(pivot.x),
-            y: f(pivot.y),
-        };
-
-        self.canvas.draw(image, DrawParam {
-            src: Rect {
-                x: atlas_rect.pos.x / image_dim.x,
-                y: atlas_rect.pos.y / image_dim.y,
-                w: atlas_rect.dim.x / image_dim.x,
-                h: atlas_rect.dim.y / image_dim.y,
-            },
-            color: Color::WHITE,
-            transform: Transform::Values {
-                dest: Point2 {
-                    x: (f(dest.x) + pivot.x - f(anchor.x)) * ps,
-                    y: (f(dest.y) + pivot.y - f(anchor.y)) * ps,
-                },
-                rotation: angle.to_rad(),
-                scale: Vector2 { x: ps, y: ps },
-                offset: Point2 {
-                    x: pivot.x / atlas_rect.dim.x,
-                    y: pivot.y / atlas_rect.dim.y,
-                },
-            },
-            z,
-        });
+        self.canvas.draw(image, DrawParam::default().src(GraphicsRect {
+            x: atlas_rect_f.origin.x    / image_size_f.width,
+            y: atlas_rect_f.origin.y    / image_size_f.height,
+            w: atlas_rect_f.size.width  / image_size_f.width,
+            h: atlas_rect_f.size.height / image_size_f.height,
+        }).dest([
+            (dest_f.x + pivot_f.x - anchor_f.x) * ps,
+            (dest_f.y + pivot_f.y - anchor_f.y) * ps,
+        ]).rotation(
+            angle.to_rad()
+        ).scale(
+            [ ps, ps ]
+        ).offset([
+            pivot_f.x / atlas_rect_f.size.width,
+            pivot_f.y / atlas_rect_f.size.height,
+        ]).z(
+            z
+        ));
 
     }
 
